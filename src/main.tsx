ea7628276e723997d929d6061139c004740bea36
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2023-03-24 01:05:57 +0200

    djustment

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
-import "react-date-range/dist/styles.css"; // main style file
-import "react-date-range/dist/theme/default.css"; // theme css file
-import "@reach/dialog/styles.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
